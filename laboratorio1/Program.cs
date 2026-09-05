a0c045bf187c614cb5ec1ed90ccbb4edeca705e8
ElyGSR <elygabrielsanchezrossell@gmail.com>
2026-09-04 22:21:01 -0500

    Primer commit

---
+++ laboratorio1/Program.cs
@@ -0,0 +1,17 @@
+﻿int x = 0;
+while (x == 0)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        System.Console.WriteLine($"Ingrese nota {i + 1}: ");
+        int nota = int.Parse(Console.ReadLine());
+    }
+
+    System.Console.WriteLine("¿Deseas ingresar notas de otro estudiante? (s/n): ");
+    char estudiante = char.Parse(Console.ReadLine());
+
+    if (estudiante == 's') 
+        x = 0;
+    else 
+        x = 1;
+}
